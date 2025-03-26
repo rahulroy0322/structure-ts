61e9422937dffa97ed4bd09682ae5502f80971ca
rahulroy0322 <rahulroyapd80@gmail.com>
2025-03-26 14:15:10 +0530

    fix(config): configure subject, type and scope for commitlint

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,3 +1,22 @@
+import type { UserConfig } from '@commitlint/types';
+
+const errorCode = 2;
+
+const typeEnum = [
+  'build', //Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)
+  'docs', //Documentation only changes
+  'feat', //A new feature
+  'fix', //A bug fix
+  'perf', //A code change that improves performance
+  'refactor', //A code change that neither fixes a bug nor adds a feature
+  'style', //Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+  'test', //Adding missing tests or correcting existing tests
+];
+
 export default {
   extends: ['@commitlint/config-conventional'],
-};
+  rules: {
+    'type-enum': [errorCode, 'always', typeEnum],
+    'scope-enum': [errorCode, undefined as unknown as 'always', typeEnum],
+  },
+} satisfies UserConfig;
